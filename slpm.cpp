455473671e4eb0a3dc947169d801b9f8730931a0
Laszlo Ashin <laszlo@ashin.hu>
2017-03-19 11:57:24 +0100

    Implement proper ssh key cache management

--- slpm.cpp
+++ slpm.cpp
@@ -163,12 +163,6 @@ append(Buffer<uint8_t, 4096>& result, const Ed25519PublicKey& pk)
 }
 
 struct SshAgent {
-	~SshAgent()
-	{
-		if (!valid_) return;
-		std::for_each(keys_.begin(), keys_.end(), [this](auto& k){ if (k) this->remove(*k); });
-	}
-
 	SshAgent()
 	: fd_(socket(AF_UNIX, SOCK_STREAM, 0))
 	{
@@ -198,64 +192,130 @@ struct SshAgent {
 	int
 	add(const Ed25519KeyPair& k)
 	{
-		if (!valid_) return -1;
-		Buffer<uint8_t, 4096> buf;
-		buf.append_network_long(0);
-		buf += '\x19'; // SSH2_AGENTC_ADD_ID_CONSTRAINED
-		buf.append_with_be32_length_prefix("ssh-ed25519");
-		buf.append_with_be32_length_prefix(reinterpret_cast<const char*>(k.pub.data()), k.pub.size());
-		buf.append_with_be32_length_prefix(reinterpret_cast<const char*>(k.sec.data()), k.sec.size());
-		buf.append_with_be32_length_prefix("comment");
-		buf += '\x01'; // SSH_AGENT_CONSTRAIN_LIFETIME
-		buf.append_network_long(86400);
-		buf += '\x02'; // SSH_AGENT_CONSTRAIN_CONFIRM
-		*reinterpret_cast<uint32_t*>(buf.data()) = htonl(buf.size() - 4);
-		buf.write(fd_.get()); // TODO: check return value
-		std::array<uint8_t, 8> resp;
-		const auto rd = read(fd_.get(), resp.data(), resp.size());
-		if (rd != 5 || ntohl(*reinterpret_cast<uint32_t*>(resp.data())) != 1) {
-			writes(STDERR_FILENO, "Unexpected result size from ssh-agent\n");
-			return -2;
+		if (exists(k.pub)) {
+			writes(STDERR_FILENO, "Key was already in agent\n");
+			return 0;
 		}
-		if (keys_[n_]) remove(*keys_[n_]);
-		keys_[n_] = k.pub;
-		n_ = (n_ + 1) % keys_.size();
-		return (resp[4] == 6) ? 0 : -3;
+		Entry e(fd_, k);
+		if (e.error()) return e.error();
+		auto& item = entries_[n_];
+		if (item) {
+			writes(STDERR_FILENO, "Oldest key is evicted from agent\n");
+		}
+		item = std::move(e);
+		n_ = (n_ + 1) % entries_.size();
+		return 0;
 	}
 
 private:
-	using Key = Ed25519PublicKey;
-	using OptKey = std::experimental::optional<Key>;
-	using Keys = std::array<OptKey, 8>;
-
-	int
-	remove(const Ed25519PublicKey& pk)
-	{
-		if (!valid_) return -1;
-		Buffer<uint8_t, 4096> buf;
-		buf.append_network_long(0);
-		buf += '\x12'; // SSH2_AGENTC_REMOVE_IDENTITY
-		buf.append_network_long(0x33);
-		buf.append_with_be32_length_prefix("ssh-ed25519");
-		buf.append_with_be32_length_prefix(reinterpret_cast<const char*>(pk.data()), pk.size());
-		*reinterpret_cast<uint32_t*>(buf.data()) = htonl(buf.size() - 4);
-		buf.write(fd_.get()); // TODO: check return value
-		std::array<uint8_t, 8> resp;
-		const auto rd2 = read(fd_.get(), resp.data(), resp.size());
-		if (rd2 != 5 || ntohl(*reinterpret_cast<uint32_t*>(resp.data())) != 1) {
-			writes(STDERR_FILENO, "Unexpected result size from ssh-agent at removing key\n");
-			return -2;
+	struct Entry {
+		~Entry()
+		{
+			if (error()) return;
+			Buffer<uint8_t, 4096> buf;
+			buf.append_network_long(0);
+			buf += '\x12'; // SSH2_AGENTC_REMOVE_IDENTITY
+			buf.append_network_long(0x33);
+			buf.append_with_be32_length_prefix("ssh-ed25519");
+			buf.append_with_be32_length_prefix(reinterpret_cast<const char*>(pk_.data()), pk_.size());
+			sodium_memzero(pk_.data(), pk_.size());
+			*reinterpret_cast<uint32_t*>(buf.data()) = htonl(buf.size() - 4);
+			buf.write(fd_->get()); // TODO: check return value
+			std::array<uint8_t, 8> resp;
+			const auto rd2 = read(fd_->get(), resp.data(), resp.size());
+			if (rd2 != 5 || ntohl(*reinterpret_cast<uint32_t*>(resp.data())) != 1) {
+				writes(STDERR_FILENO, "Unexpected result size from ssh-agent at removing key\n");
+				return;
+			}
+			if (resp[4] != 6) {
+				writes(STDERR_FILENO, "ssh-agent did not return success at removing key\n");
+				return;
+			}
 		}
-		return (resp[4] == 6) ? 0 : -3;
+
+		Entry(Fd& fd, const Ed25519KeyPair& k)
+		: fd_(&fd)
+		, pk_(k.pub)
+		{
+			if (!fd_->valid()) return;
+			Buffer<uint8_t, 4096> buf;
+			buf.append_network_long(0);
+			buf += '\x19'; // SSH2_AGENTC_ADD_ID_CONSTRAINED
+			buf.append_with_be32_length_prefix("ssh-ed25519");
+			buf.append_with_be32_length_prefix(reinterpret_cast<const char*>(k.pub.data()), k.pub.size());
+			buf.append_with_be32_length_prefix(reinterpret_cast<const char*>(k.sec.data()), k.sec.size());
+			buf.append_with_be32_length_prefix("comment");
+			buf += '\x01'; // SSH_AGENT_CONSTRAIN_LIFETIME
+			buf.append_network_long(86400);
+			buf += '\x02'; // SSH_AGENT_CONSTRAIN_CONFIRM
+			*reinterpret_cast<uint32_t*>(buf.data()) = htonl(buf.size() - 4);
+			buf.write(fd_->get()); // TODO: check return value
+			std::array<uint8_t, 8> resp;
+			const auto rd = read(fd_->get(), resp.data(), resp.size());
+			if (rd != 5 || ntohl(*reinterpret_cast<uint32_t*>(resp.data())) != 1) {
+				writes(STDERR_FILENO, "Unexpected result size from ssh-agent\n");
+				return;
+			}
+			if (resp[4] == 6) error_ = 0;
+		}
+
+		Entry(const Entry&) = delete;
+
+		Entry(Entry&& e)
+		: fd_(e.fd_)
+		, pk_(e.pk_)
+		, error_(e.error_)
+		{
+			e.fd_ = nullptr;
+			sodium_memzero(e.pk_.data(), e.pk_.size());
+			e.error_ = -1;
+		}
+
+		Entry&
+		operator=(Entry e)
+		{
+			using std::swap;
+			swap(fd_, e.fd_);
+			swap(pk_, e.pk_);
+			swap(error_, e.error_);
+			return *this;
+		}
+
+		int error() const { return error_; }
+
+		bool
+		operator==(const Ed25519PublicKey& rhs)
+		const
+		{
+			return !sodium_memcmp(pk_.data(), rhs.data(), pk_.size());
+		}
+
+	private:
+		Fd* fd_;
+		Ed25519PublicKey pk_;
+		int error_ = -1;
+	};
+
+	using OptEntry = std::experimental::optional<Entry>;
+	using Entries = std::array<OptEntry, 8>;
+
+	bool
+	exists(const Ed25519PublicKey& pk)
+	const
+	{
+		return std::find_if(
+			  entries_.begin()
+			, entries_.end()
+			, [&](const auto& e){ return e && *e == pk; }
+		) != entries_.end();
 	}
 
 	Fd fd_;
-	Keys keys_;
+	Entries entries_;
 	bool valid_{};
 	int n_{};
 };
 
-// TODO: remove keys from ssh-agent when exiting slpm
 // TODO: fill in comment field properly
 // TODO: fill in user@localhost properly
 
@@ -383,6 +443,20 @@ sodium_memzero(void * const pnt, const size_t len)
 	size_t i = (size_t) 0U;
 
 	while (i < len) pnt_[i++] = 0U;
+}
+
+int
+sodium_memcmp(const void * const b1_, const void * const b2_, size_t len)
+{
+	const volatile unsigned char *volatile b1 = (const volatile unsigned char * volatile) b1_;
+	const volatile unsigned char *volatile b2 = (const volatile unsigned char * volatile) b2_;
+	size_t i;
+	unsigned char d = (unsigned char) 0U;
+
+	for (i = 0U; i < len; i++) {
+		d |= b1[i] ^ b2[i];
+	}
+	return (1 & ((d - 1) >> 8)) - 1;
 }
 
 int
