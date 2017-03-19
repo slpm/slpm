8f25e365ccca9f47b04dbe5979a40d4f16129e06
Laszlo Ashin <laszlo@ashin.hu>
2017-03-19 12:47:35 +0100

    Test ssh

--- check.sh
+++ check.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 export SLPM_FULLNAME="John Doe"
-./slpm.comp << EOF | diff -u3 expected-output /dev/stdin
+export USER="jdoe"
+ssh-agent ./slpm.comp << EOF | diff -u3 expected-output /dev/stdin
 correct horse battery staple
 twitter.com
 1
 facebook.com
 2
+ssh github.com
+1
 EOF
