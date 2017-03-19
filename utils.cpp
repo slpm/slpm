4f31f1a3b15302a0373e3a6cab8c38bb698308b0
Laszlo Ashin <laszlo@ashin.hu>
2017-03-19 13:22:43 +0100

    Implement getenv_or()

--- utils.cpp
+++ utils.cpp
@@ -1,9 +1,17 @@
 #include "utils.h"
 
 #include <cstring>
+#include <cstdlib>
 
 ssize_t
 writes(int fd, const char* s)
 {
 	return write(fd, s, std::strlen(s));
+}
+
+const char*
+getenv_or(const char* name, const char* _default)
+{
+	const char *const value = std::getenv(name);
+	return value ? value : _default;
 }
